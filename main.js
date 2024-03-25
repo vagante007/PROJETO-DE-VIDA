4a50f8dc6d3d163e31916674f2cc7d29340716d3
vagante007 <106813312+vagante007@users.noreply.github.com>
2024-03-25 11:53:55 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-botoes[j].classList.remove("ativo");
-textos[j].classList.remove("ativo");
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
@@ -16,3 +12,8 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
